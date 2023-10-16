0a500599ff1950f588ea2e13a95356a1fb127616
Rafael Alves <rafael.allves91@gmail.com>
2023-10-16 16:05:24 -0300

    get positions

--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from app.models import AssetPrice
+from .models import Order
+import pandas as pd
 
-# Create your views here.
+
+def position(request, user):
+  orders = Order.objects.filter(user=user)
+  assets = {}
+  for order in orders:
+    asset_name = order.name
+    volume = order.volume
+    cost = order.price
+
+    if asset_name in assets:
+        assets[asset_name]["volume"] += int(volume)
+        assets[asset_name]["cost"] += cost
+    else:
+        latest_price = AssetPrice.objects.filter(ticker = asset_name).last()
+        assets[asset_name] = {
+            "name": asset_name,
+            "volume": int(volume),
+            "cost": cost,
+            "price": latest_price.close if latest_price else None
+        }
+
+  df = pd.DataFrame(assets.values())
+  print(df.values.tolist())
+
+  return JsonResponse(df.values.tolist(), safe=False)
+
