de5b903d44b83fc70afe32e10fa1b8f2f69e8061
Rafael Alves <rafael.allves91@gmail.com>
2023-10-22 17:33:06 -0300

    Patrimony chart for ticker

--- src/app/tickers/[ticker]/page.tsx
+++ src/app/tickers/[ticker]/page.tsx
@@ -1,13 +1,31 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Stock from '../../../components/charts/stock';
+import { Patrimony } from '../../../components/charts/patrimony';
 
 const TickerChart: React.FC<any> = ({ params }) => {
   const { ticker } = params;
+  const [data, setData] = useState<any>([]);
+  const [dataPatrimony, setDataPatrimony] = useState<any>([]);
+
+  useEffect(()=> {
+    axios.get(`http://127.0.0.1:8000/position/1/${ticker}`).then(response => {
+      setData(response.data)
+    })
+    axios.get(`http://127.0.0.1:8000/position-history/1`, {params: {ticker}}).then(response => {
+      setDataPatrimony(response.data)
+    })
+  }, [ticker])
 
   return (
-    <Stock ticker={ticker}/>
+    <main className="flex h-screen flex-col">
+      <div  className="flex h-1/2 w-1/2">
+        <Patrimony data={dataPatrimony}/>
+      </div>
+      <Stock ticker={ticker}/>
+    </main>
   );
 };
 
