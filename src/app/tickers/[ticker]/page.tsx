08eb7eda223195ac078f9465e61cb083d3df96de
Rafael Alves <rafael.allves91@gmail.com>
2023-10-26 14:33:43 -0300

    Add purchase points

--- src/app/tickers/[ticker]/page.tsx
+++ src/app/tickers/[ticker]/page.tsx
@@ -9,20 +9,26 @@ const TickerChart: React.FC<any> = ({ params }) => {
   const { ticker } = params;
   const [dataPatrimony, setDataPatrimony] = useState<any>([]);
   const [dataStock, setDataStock] = useState<any>([]);
-  const [startDate, setStartDate] = useState<any>([]);
+  const [dataOrders, setDataOrders] = useState<any>([]);
+  const [startDate, setStartDate] = useState<any>();
 
   useEffect(()=> {
     axios.get(`http://127.0.0.1:8000/position-history/1`, {params: {ticker}}).then(response => {
       setDataPatrimony(response.data)
       setStartDate(Date.UTC(
         parseInt(response.data?.labels?.[0].split('/')[2], 10), 
-        parseInt(response.data?.labels?.[0].split('/')[1], 10), 
+        parseInt(response.data?.labels?.[0].split('/')[1], 10)-1, 
         parseInt(response.data?.labels?.[0].split('/')[0], 10),
       ))
     })
+
     axios.get(`http://127.0.0.1:8000/asset/${ticker}`).then(response => {
       setDataStock(response.data)
+    })
 
+    axios.get(`http://127.0.0.1:8000/orders`, {params: {ticker}}).then(response => {
+      setDataOrders(response.data)
+      console.log(response.data?.map(e => [e[13], e[6]]))
     })
   }, [ticker])
 
@@ -31,7 +37,7 @@ const TickerChart: React.FC<any> = ({ params }) => {
       <div  className="flex h-1/2 w-1/2">
         <Patrimony data={dataPatrimony}/>
       </div>
-      <Stock data={dataStock} startDate={startDate}/>
+      <Stock data={dataStock} dataOrders={dataOrders} startDate={startDate}/>
     </main>
   );
 };
