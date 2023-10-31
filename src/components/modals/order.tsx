1b2bc1a68de1db7f6abebe7436d9cde8347c33bf
Rafael Alves <rafael.allves91@gmail.com>
2023-10-31 13:55:55 -0300

    Make fields mandatory

--- src/components/modals/order.tsx
+++ src/components/modals/order.tsx
@@ -4,23 +4,32 @@ const OrderModal: React.FC<{
   onClose: () => void;
   onSave: (data: any) => void;
 }> = ({ onClose, onSave }) => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<any>({
     name: "",
     broker: "Nu Invest",
     assetType: "AC",
     orderType: "buy",
-    date: "",
-    price: "",
-    volume: "",
+    date: null,
+    price: null,
+    volume: null,
     description: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    if (name === "name") {
+      setFormData({ ...formData, [name]: value.toUpperCase() });
+    } else if (name === "price") {
+      setFormData({ ...formData, [name]: value? Number(value) : null });
+    } else if (name === "volume") {
+      setFormData({ ...formData, [name]: value? Number(value) : null });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
-  const handleSave = () => {
+  const handleSave = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
     onSave(formData);
     onClose();
   };
@@ -29,11 +38,11 @@ const OrderModal: React.FC<{
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black z-50 text-black">
       <div className="bg-white p-4 rounded-md shadow-md lg:w-2/5 lg:space-x-4">
         <h2 className="text-2xl font-bold mb-4">Boleta</h2>
-        <form className="lg:flex lg:flex-wrap">
+        <form className="lg:flex lg:flex-wrap" onSubmit={handleSave}>
 
           <div className="mb-4 lg:w-1/2 p-4">
             <label htmlFor="broker" className="block text-sm font-semibold mb-2">Selecione a corretora:</label>
-            <select id="broker" name="broker" value={formData.broker} onChange={handleChange} className="w-full px-4 py-2 border rounded-md">
+            <select id="broker" name="broker" required value={formData.broker} onChange={handleChange} className="w-full px-4 py-2 border rounded-md">
               <option value="Nu Invest">Nu Invest</option>
               <option value="Rico">Rico</option>
               <option value="XP">XP</option>
@@ -42,7 +51,7 @@ const OrderModal: React.FC<{
 
           <div className="mb-4 lg:w-1/2 p-4">
             <label htmlFor="assetType" className="block text-sm font-semibold mb-2">Tipo:</label>
-            <select id="assetType" name="assetType" value={formData.assetType} onChange={handleChange} className="w-full px-4 py-2 border rounded-md">
+            <select id="assetType" name="assetType" required value={formData.assetType} onChange={handleChange} className="w-full px-4 py-2 border rounded-md">
               <option value="AC">Ações BR</option>
               <option value="INT">Ativos Internacionais</option>
               <option value="FII">Fundos de Investimento Imobiliário</option>
@@ -59,6 +68,7 @@ const OrderModal: React.FC<{
               id="name"
               value={formData.name}
               onChange={handleChange}
+              required
               className="w-full px-4 py-2 border rounded-md"
             />
           </div>
@@ -99,6 +109,7 @@ const OrderModal: React.FC<{
               id="date"
               value={formData.date}
               onChange={handleChange}
+              required
               className="w-full px-4 py-2 border rounded-md"
             />
           </div>
@@ -108,11 +119,12 @@ const OrderModal: React.FC<{
               Preço:
             </label>
             <input
-              type="text"
+              type="number"
               name="price"
               id="price"
               value={formData.price}
               onChange={handleChange}
+              required
               className="w-full px-4 py-2 border rounded-md"
             />
           </div>
@@ -122,10 +134,11 @@ const OrderModal: React.FC<{
               Volume:
             </label>
             <input
-              type="text"
+              type="number"
               name="volume"
               id="volume"
               value={formData.volume}
+              required
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-md"
             />
@@ -147,8 +160,7 @@ const OrderModal: React.FC<{
           
           
           <button
-            type="button"
-            onClick={handleSave}
+            type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
           >
             Salvar
