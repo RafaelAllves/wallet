3895cd5ecb898d99fac9f628e72a1509715f4066
Rafael Alves <rafael.allves91@gmail.com>
2023-10-31 13:19:22 -0300

    Structure for creating new orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,11 +2,26 @@
 
 import React, { useEffect, useState } from 'react';
 import OrdersTable from '../../components/ordersTable';
+import OrderModal from '../../components/modals/order'
 import axios from 'axios';
 
 const OrdersPage: React.FC<any> = ({ params }) => {
   const { ticker } = params;
   const [dataOrders, setDataOrders] = useState<(number | string | null)[][]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const saveData = (data: any) => {
+    console.log(data);
+  };
+
   
   useEffect(()=> {
     axios.get(`http://127.0.0.1:8000/orders`, {params: {ticker}}).then(response => {
@@ -17,6 +32,10 @@ const OrdersPage: React.FC<any> = ({ params }) => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Tabela de Boletas</h1>
+      <button onClick={openModal}>Abrir Modal</button>
+      {isModalOpen && (
+        <OrderModal onClose={closeModal} onSave={saveData} />
+      )}
       <OrdersTable data={dataOrders} />
     </div>
   );
