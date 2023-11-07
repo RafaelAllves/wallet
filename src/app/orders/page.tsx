ac4e756b5730ebe026849ea643799c2b67c42749
Rafael Alves <rafael.allves91@gmail.com>
2023-11-07 17:32:07 -0300

    Create order

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -18,15 +18,22 @@ const OrdersPage: React.FC<any> = ({ params }) => {
     setIsModalOpen(false);
   };
 
-  const saveData = (data: any) => {
-    console.log(data);
-  };
-
   const getData = (ticker: string | null) => {
     axios.get(`http://127.0.0.1:8000/orders`, {params: {ticker}}).then(response => {
       setDataOrders(response.data)
     })
   }
+
+  const saveData = (data: any) => {
+    axios.post(`http://127.0.0.1:8000/order`, {params: data})
+      .then(response => {
+        getData(null)
+        alert('Boleta criada com sucesso')
+      })
+      .catch(error => {
+        alert('Erro ao criar boleta')
+      })
+  };
 
   useEffect(()=> {
     getData(ticker)
