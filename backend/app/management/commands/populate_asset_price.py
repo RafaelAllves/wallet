84c36809edf2f806e7f175afa6569fe19c98d6a7
Rafael Alves <rafaelsralves91@gmail.com>
2024-06-04 19:53:03 -0300

    lint

--- backend/app/management/commands/populate_asset_price.py
+++ backend/app/management/commands/populate_asset_price.py
@@ -5,66 +5,145 @@ from app.models import AssetPrice
 import time
 from datetime import datetime, timedelta
 
+
 class Command(BaseCommand):
-  help = 'Este é um comando personalizado do Django.'
+    help = "Este é um comando personalizado do Django."
 
+    def handle(self, *args, **options):
 
+        def get_standard_lot(start, end):
+            response = requests.request(
+                "GET",
+                f"https://arquivos.b3.com.br/bdi/table/StandardLot/{start}/{end}/1/2000",
+            )
+            if response.text:
+                try:
+                    data = json.loads(response.text)
+                except json.JSONDecodeError:
+                    print(f"Invalid JSON response: {response.text}")
+                    data = {}
+            else:
+                print("Empty response")
+                return
 
-  def handle(self, *args, **options):
+            print("get standard lot")
 
-    def get_standard_lot(start, end):
-      response = requests.request("GET", f"https://arquivos.b3.com.br/bdi/table/StandardLot/{start}/{end}/1/2000")
-      data = json.loads(response.text)
+            new_data = []
 
-      print('get standard lot')
+            if data["table"] and data["table"]["values"]:
+                for asset_data in data["table"]["values"]:
+                    (
+                        ticker,
+                        trading_name,
+                        cod,
+                        open,
+                        low,
+                        high,
+                        mid,
+                        close,
+                        oscillation,
+                        pch_pric,
+                        sl_pric,
+                        num,
+                        trad_qty,
+                        offers,
+                        trades,
+                        aux,
+                    ) = asset_data
 
-      new_data = [] 
+                    if (
+                        isinstance(close, float)
+                        and not AssetPrice.objects.filter(
+                            ticker=ticker, date=end
+                        ).exists()
+                    ):
+                        new_asset_price = AssetPrice(
+                            ticker=ticker,
+                            date=end,
+                            open=open,
+                            low=low,
+                            high=high,
+                            mid=mid,
+                            close=close,
+                        )
+                        new_data.append(new_asset_price)
 
-      if data["table"] and data["table"]['values']:
-        for asset_data in data["table"]['values']:
-          ticker, trading_name, cod, open, low, high, mid, close, oscillation, pch_pric, sl_pric, num, trad_qty, offers, trades, aux = asset_data
+                print("standard lot:   ", len(new_data))
+                AssetPrice.objects.bulk_create(new_data)
 
-          if isinstance(close, float) and not AssetPrice.objects.filter(ticker=ticker, date=end).exists():
-            new_asset_price = AssetPrice(ticker=ticker, date=end, open=open, low=low, high=high, mid=mid, close=close)
-            new_data.append(new_asset_price)
-      
-        print('standard lot:   ', len(new_data))
-        AssetPrice.objects.bulk_create(new_data)
+        def get_real_estate_funds(start, end):
+            response = requests.request(
+                "GET",
+                f"https://arquivos.b3.com.br/bdi/table/RealEstateFunds/{start}/{end}/1/2000",
+            )
+            if response.text:
+                try:
+                    data = json.loads(response.text)
+                except json.JSONDecodeError:
+                    print(f"Invalid JSON response: {response.text}")
+                    data = {}
+            else:
+                print("Empty response")
+                return
 
+            print("get real estate funds")
 
+            new_data = []
 
+            if data["table"] and data["table"]["values"]:
+                for asset_data in data["table"]["values"]:
+                    (
+                        ticker,
+                        trading_name,
+                        cod,
+                        open,
+                        low,
+                        high,
+                        mid,
+                        close,
+                        oscillation,
+                        pch_pric,
+                        sl_pric,
+                        num,
+                        trad_qty,
+                        offers,
+                        trades,
+                        aux,
+                    ) = asset_data
 
+                    if (
+                        isinstance(close, float)
+                        and not AssetPrice.objects.filter(
+                            ticker=ticker, date=end
+                        ).exists()
+                    ):
+                        new_asset_price = AssetPrice(
+                            ticker=ticker,
+                            date=end,
+                            open=open,
+                            low=low,
+                            high=high,
+                            mid=mid,
+                            close=close,
+                        )
+                        new_data.append(new_asset_price)
 
-    
-    def get_real_estate_funds(start, end):
-      response = requests.request("GET", f"https://arquivos.b3.com.br/bdi/table/RealEstateFunds/{start}/{end}/1/2000")
-      data = json.loads(response.text)
+                print("real estate:   ", len(new_data))
+                AssetPrice.objects.bulk_create(new_data)
 
-      print('get real estate funds')
+        # data_inicio = datetime(2023, 10, 8)
+        # data_fim = datetime(2023, 10, 10)
 
-      new_data = [] 
+        data_inicio = datetime(2024, 4, 20)
+        data_fim = datetime(2024, 5, 2)
 
-      if data["table"] and data["table"]['values']:
-        for asset_data in data["table"]['values']:
-          ticker, trading_name, cod, open, low, high, mid, close, oscillation, pch_pric, sl_pric, num, trad_qty, offers, trades, aux = asset_data
+        while data_inicio <= data_fim:
 
-          if isinstance(close, float) and not AssetPrice.objects.filter(ticker=ticker, date=end).exists():
-            new_asset_price = AssetPrice(ticker=ticker, date=end, open=open, low=low, high=high, mid=mid, close=close)
-            new_data.append(new_asset_price)
-      
-        print('real estate:   ', len(new_data))
-        AssetPrice.objects.bulk_create(new_data)
+            date = data_inicio.strftime("%Y-%m-%d")
+            print(date)
+            get_standard_lot(date, date)
+            get_real_estate_funds(date, date)
 
-    data_inicio = datetime(2023, 10, 8)
-    data_fim = datetime(2023, 10, 10)
+            time.sleep(3)
 
-    while data_inicio <= data_fim:
-
-      date = data_inicio.strftime("%Y-%m-%d")
-      print(date)
-      get_standard_lot(date, date)
-      get_real_estate_funds(date, date)
-      
-      time.sleep(3)
-
-      data_inicio += timedelta(days=1)
+            data_inicio += timedelta(days=1)
