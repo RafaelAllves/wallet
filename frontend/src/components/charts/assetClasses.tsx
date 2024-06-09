9c9250efb1e6911d684d77477e0f6d73a885a657
Rafael Alves <rafaelsralves91@gmail.com>
2024-06-09 08:37:58 -0300

    feat: Change label color

--- frontend/src/components/charts/assetClasses.tsx
+++ frontend/src/components/charts/assetClasses.tsx
@@ -20,35 +20,47 @@ interface AssetClassesProps {
   }
 }
 
-export const AssetClasses: React.FC<AssetClassesProps> = ({asset_classes}) => {
+export const AssetClasses: React.FC<AssetClassesProps> = ({ asset_classes }) => {
 
-  if(!asset_classes) return;
+  if (!asset_classes) return;
 
-  return <Doughnut data={{
-    labels: Object.keys(asset_classes),
-    datasets: [
-      {
-        label: 'Valor Acumulado',
-        data: Object.values(asset_classes).map(e => e.value),
-        
-        backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
+  return <Doughnut
+    data={{
+      labels: Object.keys(asset_classes),
+      datasets: [
+        {
+          label: 'Valor Acumulado',
+          data: Object.values(asset_classes).map(e => e.value),
+
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    }}
+
+    options={{
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white',
+          },
+        },
       },
-    ],
-  }} />;
+    }}
+  />;
 }
