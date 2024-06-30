c3df702341a7752e5a86a365a5750c0ae964aff0
Rafael Alves <rafaelsralves91@gmail.com>
2024-06-30 12:53:02 -0300

    feat: Add token to authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "user",
     "wallet",
     "corsheaders",
+    "rest_framework.authtoken",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -139,3 +140,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
--- backend/user/views.py
+++ backend/user/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
 
 
 @csrf_exempt
@@ -13,7 +14,20 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse("Login efetuado com sucesso")
+            token, created = Token.objects.get_or_create(user=user)
+            return HttpResponse(
+                json.dumps(
+                    {
+                        "token": token.key,
+                        "user": {
+                            "id": user.id,
+                            "username": user.username,
+                            "email": user.email,
+                        },
+                    }
+                ),
+                content_type="application/json",
+            )
         else:
             return HttpResponseBadRequest("Usuário ou senha inválidos")
     return HttpResponseBadRequest("Método não permitido")
