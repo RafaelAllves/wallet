359f49cc3d86a8d27fae45e80f2c9d2af5073e29
Rafael Alves <rafaelsralves91@gmail.com>
2024-07-09 17:48:03 -0300

    refactor: Update navbar component to include logout button

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,10 +2,19 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
+import { useAuth } from '@/services/authContext';
 
 const Navbar = () => {
   const pathname = usePathname();
+  const { logout } = useAuth();
+
   if (pathname === '/auth') return null;
+
+  const handleLogout = () => {
+    logout();
+    window.location.href = '/auth';
+  };
+
 
   return (
     <nav className="fixed top-0 left-0 right-0 bg-gray-800 p-4 text-white">
@@ -17,6 +26,7 @@ const Navbar = () => {
         <div className="space-x-4">
           <Link href="/orders">Boletas</Link>
           <Link href="/tickers">Tickers</Link>
+          <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </nav>
